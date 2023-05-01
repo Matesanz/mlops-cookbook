6c051beac15fffd71037c86c272704f21aa6af22
Andrés Matesanz Cuadrado <andres.matesanz@sngular.com>
2023-05-01 21:46:16 +0000

    🔧 use MLFLOW_TRACKING_URI env variable

--- mlops_course/settings.py
+++ mlops_course/settings.py
@@ -6,7 +6,9 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     """Base Settings used to configure the project."""
 
-    MLFLOW_URI: str = "http://mlflow:5000"
+    MLFLOW_TRACKING_URI: str = "http://mlflow:5000"
+    MLFLOW_EXPERIMENT_NAME: str = "mlops-course"
+    DATA_PATH: str = "data/"
 
 
 config = Settings()

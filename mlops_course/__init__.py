c27c6a9c8144131eabda7ae5db07651ac9de6655
Andrés Matesanz Cuadrado <andres.matesanz@sngular.com>
2023-05-16 18:24:04 +0000

    📝 update __init__ documentation

--- mlops_course/__init__.py
+++ mlops_course/__init__.py
@@ -1,7 +1,10 @@
-"""Root module for mlops-course."""
+"""This app allows to predict survival on the Titanic.
+
+This project is a simple API that allows users to input their personal
+information and get a prediction of their chances of surviving the
+Titanic disaster. It also has a Streamlit app that allows users to
+interact with the API.
+"""
 from .settings import config
 
 __version__ = "0.4.0"
-
-
-__all__ = ["config", "__version__"]
